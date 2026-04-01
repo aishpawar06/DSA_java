69f2d9a00d4d51095dbc6daf65ca4628fe619360
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-04-02 02:21:17 +0530

    added linear search implementation

---
+++ Arrays/LinearSearch.java
@@ -0,0 +1,27 @@
+/*
+    Problem : Linear Search
+    Platform : BAsic DSA/ Self Practice
+    Approach : Traverse the array one by one and compare each element
+    TC : O(N)
+    SC : O(1)
+ */
+
+
+package Arrays;
+public class LinearSearch{
+    public static int search(int[] arr, int key){
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == key){
+                return i;
+            }
+        }
+        return -1;
+
+    }
+    public static void main(String[] args){
+        int[] array = {1,2,4,5,6,3,9,10};
+        int key = 9;
+        System.out.println("Element found at index : " + search(array, key));
+
+    }
+}
