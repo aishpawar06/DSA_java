54ab24a2a7e65ced932219892efe4787ee014e12
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-04-05 19:37:04 +0530

    added max consecutive ones problem (counting consecutive elements)

---
+++ Arrays/MaxConsecutiveOnes.java
@@ -0,0 +1,34 @@
+/*
+    Problem: Max Consecutive Ones
+    Platform: LeetCode
+    Level: Easy
+    Approach: Traverse array and count consecutive 1s, reset count on 0
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+*/
+
+package Arrays;
+class MaxConsecutiveOnes{
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int count = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            if(nums[i] == 1){
+                count++;
+                if(count > max){
+                    max = count;   
+                }
+                
+            }else{
+                count = 0;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args){
+        int[] arr = {1,1,0,1,1,1};
+        System.out.println("Max consecutive 1's : " + findMaxConsecutiveOnes(arr));
+    }
+
+}
