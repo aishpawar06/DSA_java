098ec998b38c8db5c4cc6768989770d1ed273510
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-04-08 00:21:06 +0530

    added rotate array using reversal algorithm (O(n), O(1))

---
+++ Arrays/RotateArrayRight.java
@@ -0,0 +1,38 @@
+/*
+    Problem: Right Rotate Array by k elements
+    Platform: LeetCode 189
+    Level: Medium
+    Approach: Reverse parts of array to achieve rotation
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+*/
+
+package Arrays;
+class RotateArrayRight {
+    public static void reverse(int st, int end, int[] arr){
+        while(st < end){
+            int temp = arr[st];
+            arr[st] = arr[end];
+            arr[end] = temp;
+            st++;
+            end--;
+        }
+    }
+    public static void rotate(int[] nums, int k) {
+        if(nums.length == 0) return;
+        k=k%nums.length;
+        reverse(nums.length-k , nums.length-1, nums);
+        reverse(0, nums.length-k-1, nums);
+        reverse(0,nums.length-1,nums);
+    }
+
+    public static void main(String[] args){
+        int arr[] = {1,2,3,4,5,6,7};
+        rotate(arr,3);
+
+        for(int i = 0 ; i < arr.length ; i++){
+            System.out.print(" " + arr[i]);
+        }
+
+    }
+}
