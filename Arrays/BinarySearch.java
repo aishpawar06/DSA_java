5e310b9a5e046fd8efd7beb062213c5761d03ab7
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-04-08 00:39:55 +0530

    added binary search using iterative approach (O(log n))

---
+++ Arrays/BinarySearch.java
@@ -0,0 +1,42 @@
+/*
+    Problem: Binary Search
+    Platform: LeetCode 704
+    Level: Easy
+    Approach: Iterative binary search on sorted array
+    Time Complexity: O(log n)
+    Space Complexity: O(1)
+*/
+
+package Arrays;
+
+public class BinarySearch {
+    public static int search(int[] nums, int target) {
+        int st = 0;
+        int end = nums.length-1;
+        while(st<=end){
+            int mid = st + (end - st)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(target < nums[mid]){
+                end = mid - 1;
+            }
+            else{
+                st = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args){
+        int[] arr = {-1,0,3,5,9,12};
+        int result = search(arr, 3);
+        if(result == -1){
+            System.out.println("Target not found!");
+        }else{
+            System.out.println("Target found at index: " + result);
+
+        }
+    }
+}
+
