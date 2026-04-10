817da671bd20b2527b89eb0f651d01a1d06fbe9c
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-04-10 13:22:32 +0530

    added single number using XOR approach (O(n), O(1))

---
+++ Arrays/SingleNumber.java
@@ -0,0 +1,28 @@
+/*
+    Problem: Single Number
+    Platform: LeetCode
+    Level: Easy
+    Approach: XOR - duplicates cancel out
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+*/
+
+package Arrays;
+
+public class SingleNumber {
+
+    public static int singleNumber(int[] nums) {
+        int res = 0;
+
+        for(int num : nums){
+            res ^= num;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4,1,2,1,2};
+        System.out.println("Single number: " + singleNumber(nums));
+    }
+}
