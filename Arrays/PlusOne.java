ec6f29f227bd88d456461bc26dd8d99fcb726882
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-04-15 19:29:33 +0530

    added plus one using carry handling (O(n))

---
+++ Arrays/PlusOne.java
@@ -0,0 +1,34 @@
+/*
+    Problem: Plus One
+    Platform: LeetCode 66
+    Level: Easy
+    Approach: Traverse from end, handle carry by turning 9s into 0s
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+*/
+package Arrays;
+public class PlusOne{
+    public static int[] plusOne(int[] digits) {
+        for(int i = digits.length-1 ; i>=0 ; i--){
+            if(digits[i] != 9){
+                digits[i]++;
+                return digits;
+            }else{
+                digits[i] = 0;
+            }
+        }
+        digits = new int[digits.length+1];
+        digits[0] = 1;
+        return digits;
+    }
+
+    public static void main(String[] args) {
+    int[] digits = {9,9,9};
+
+    int[] result = plusOne(digits);
+
+    for(int num : result){
+        System.out.print(num + " ");
+    }
+}
+}
