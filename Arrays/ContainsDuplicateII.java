484ba68327bc5ec7e8f6207e0a659a95a90ff87a
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-04-16 19:19:18 +0530

    added contains nearby duplicate using hashmap (O(n))

---
+++ Arrays/ContainsDuplicateII.java
@@ -0,0 +1,33 @@
+/*
+    Problem: Contains Duplicate II
+    Platform: LeetCode 219
+    Level: Easy
+    Approach: HashMap to store value → last index and check distance
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+*/
+
+package Arrays;
+import java.util.*;
+public class ContainsDuplicateII{
+    public static boolean duplicates(int[] nums, int k){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0 ; i < nums.length ; i++){
+            if(map.containsKey(nums[i])){
+                if(i - map.get(nums[i]) <= k){
+                    return true;
+                }
+            }
+            map.put(nums[i],i);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1,2,3};
+        int k = 2;
+
+        System.out.println(duplicates(nums, k));
+    }
+
+}
