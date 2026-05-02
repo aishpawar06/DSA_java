8172dd785b963bf0eca55df9aa18d6d66a1b7aca
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-05-02 20:23:24 +0530

    added max profit using greedy approach (O(n))

---
+++ Arrays/MaxProfit.java
@@ -0,0 +1,32 @@
+/*
+    Problem: Best Time to Buy and Sell Stock
+    Platform: LeetCode 121
+    Level: Easy
+    Approach: Track minimum price so far and calculate max profit greedily
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+*/
+
+public class MaxProfit {
+
+    public static int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for(int i = 1; i < prices.length; i++){
+            int profit = prices[i] - minPrice;
+            maxProfit = Math.max(profit, maxProfit);
+            minPrice = Math.min(minPrice, prices[i]);
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+
+        int result = maxProfit(prices);
+
+        System.out.println("Maximum Profit: " + result);
+    }
+}
