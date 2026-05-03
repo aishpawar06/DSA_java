793c8bba7d94e660550ccc4b41651eb13899c35c
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-05-03 21:02:15 +0530

    added search insert position using binary search (O(log n))

---
+++ Arrays/SearchInsertPosition.java
@@ -0,0 +1,41 @@
+/*
+    Problem: Search Insert Position
+    Platform: LeetCode 35
+    Level: Easy
+    Approach: Binary Search – find target or return the index where it should be inserted
+    Time Complexity: O(log n)
+    Space Complexity: O(1)
+*/
+
+public class SearchInsertPosition {
+
+    public static int searchInsert(int[] nums, int target) {
+        int st = 0;
+        int end = nums.length - 1;
+
+        while(st <= end){
+            int mid = st + (end - st) / 2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                st = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+
+        return st; // insertion position
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,3,5,6};
+        int target = 5;
+
+        int result = searchInsert(nums, target);
+
+        System.out.println("Position: " + result);
+    }
+}
