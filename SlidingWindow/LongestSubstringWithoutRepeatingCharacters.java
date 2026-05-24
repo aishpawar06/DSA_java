61c8619356ab2314c33b07e7343d0cc42e5683c6
Aishwarya <aishpawar696@gmail.com>
2026-05-24 15:21:18 +0530

    implemented longest substring without repeating characters using sliding window and hashmap (O(n))

---
+++ SlidingWindow/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,26 @@
+package SlidingWindow;
+import java.util.*;
+public class LongestSubstringWithoutRepeatingCharacters{
+    public static int lengthOfLongestSubstring(String s){
+        HashMap<Character,Integer> map = new HashMap<>();
+        int lptr = 0; 
+        int max = 0;
+        for(int rptr = 0 ; rptr < s.length() ; rptr++){
+            char ch = s.charAt(rptr);
+            if(map.containsKey(ch)){
+                while(map.containsKey(ch)){
+                    map.remove(s.charAt(lptr));
+                    lptr++;
+                }
+            }
+            map.put(ch,rptr);
+            max = Math.max(max,map.size());
+        }
+        return max;
+    }
+
+    public static void main(String args[]){
+        String s = "pwwkew";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+}
