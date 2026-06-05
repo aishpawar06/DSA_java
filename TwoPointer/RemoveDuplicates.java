0e4240cc3132ff4411b52259b5a9841307008988
Aishwarya <aishpawar696@gmail.com>
2026-06-05 12:40:06 +0530

    revisited remove duplicates to reinforce two-pointer technique

--- TwoPointer/RemoveDuplicates.java
+++ TwoPointer/RemoveDuplicates.java
@@ -10,26 +10,25 @@ package TwoPointer;
 
 class RemoveDuplicates {
 
-    public static int remove(int[] nums) {
-        if(nums.length == 0) return 0;
-
-        int k = 1;
-
-        for(int i = 1; i < nums.length; i++) {
-            if(nums[i] != nums[k - 1]) {
-                nums[k] = nums[i];
-                k++;
+    public static int removeDuplicates(int[] nums) {
+        if(nums.length==0) return 0;
+        int prev = 0 ;
+        int next = 0;
+        while(next < nums.length){
+            if(nums[prev] != nums[next]){
+                nums[prev+1] = nums[next];
+                prev++;
             }
+            next++;
         }
-
-        return k;
+        return prev+1;
     }
 
     public static void main(String[] args) {
 
         int[] nums = {1, 1, 2, 2, 3, 4, 4};
 
-        int k = remove(nums);
+        int k = removeDuplicates(nums);
 
         System.out.println("Number of unique elements: " + k);
 
