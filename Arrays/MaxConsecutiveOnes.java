47cee998e5ed1c9a81c0c3bc00e15ff11f06e3c7
Aishwarya <aishpawar696@gmail.com>
2026-06-07 13:52:31 +0530

    revisited max consecutive ones using running count approach

--- Arrays/MaxConsecutiveOnes.java
+++ Arrays/MaxConsecutiveOnes.java
@@ -10,20 +10,17 @@
 package Arrays;
 class MaxConsecutiveOnes{
     public static int findMaxConsecutiveOnes(int[] nums) {
-        int max = 0;
+        int maxCount = 0;
         int count = 0;
         for(int i = 0 ; i < nums.length ; i++){
-            if(nums[i] == 1){
-                count++;
-                if(count > max){
-                    max = count;   
-                }
-                
-            }else{
+            if(nums[i] == 0){
                 count = 0;
+            }else{
+                count++;
             }
+            maxCount = Math.max(count,maxCount);
         }
-        return max;
+        return maxCount;
     }
 
     public static void main(String[] args){
