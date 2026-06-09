3b1a689b0274b4314bd50eeb1eada729c43024b3
Aishwarya <aishpawar696@gmail.com>
2026-06-09 13:38:48 +0530

    reimplemented two sum using hashmap complement lookup pattern

--- Hashing/TwoSum.java
+++ Hashing/TwoSum.java
@@ -13,13 +13,13 @@ import java.util.HashMap;
 
 public class TwoSum {
     public static int[] twoSum(int[] nums, int target){
-        HashMap<Integer,Integer> result = new HashMap<>();
+        HashMap<Integer , Integer> result = new HashMap<>();
         for(int i = 0 ; i < nums.length ; i++){
-            int needed = target-nums[i];
+            int needed = target - nums[i];
             if(result.containsKey(needed)){
                 return new int[]{result.get(needed),i};
             }
-            result.put(nums[i],i);
+            result.put(nums[i] , i);
         }
         return new int[]{-1,-1};
     }
