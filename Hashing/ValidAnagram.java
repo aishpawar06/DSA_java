7251f866b70a307888f2181f4f348adab6532b77
Aishwarya <aishpawar696@gmail.com>
2026-06-18 12:50:38 +0530

    revisited validAnagram

--- Hashing/ValidAnagram.java
+++ Hashing/ValidAnagram.java
@@ -9,16 +9,16 @@
 package Hashing;
 public class ValidAnagram{
     public static boolean Valid(String s, String t){
-        if(s.length()!=t.length()){
+        if(s.length() != t.length()){
             return false;
         }
         int[] arr = new int[26];
         for(int i = 0 ; i < s.length() ; i++){
-            arr[s.charAt(i)-'a']++;
-            arr[t.charAt(i)-'a']--;
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
         }
         for(int i = 0 ; i < 26 ; i++){
-            if(arr[i]!=0){
+            if(arr[i] != 0){
                 return false;
             }
         }
