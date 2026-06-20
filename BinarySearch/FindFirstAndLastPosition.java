1e6f81fcb7d9030f85b9fc3aa5dff99def850fe8
Aishwarya <aishpawar696@gmail.com>
2026-06-20 20:18:32 +0530

    revisited find first and last element

--- BinarySearch/FindFirstAndLastPosition.java
+++ BinarySearch/FindFirstAndLastPosition.java
@@ -11,29 +11,13 @@ public class FindFirstAndLastPosition{
 
     public static int findFirst(int[] nums, int target){
         int ans = -1;
-        int st=0,end = nums.length;
-        while(st<=end){
-            int mid = st + (end-st)/2;
+        int st = 0 ;
+        int end = nums.length-1;
+        while(st <= end){
+            int mid = st + (end - st)/2;
             if(target == nums[mid]){
                 ans = mid;
                 end = mid - 1;
-            }else if(target < nums[mid]){
-                end = mid - 1;
-            }else{
-                st = mid + 1;
-            }
-        }
-        return ans;
-    }
-    
-    public static int findLast(int[] nums, int target){
-        int ans = -1;
-        int st=0,end = nums.length;
-        while(st<=end){
-            int mid = st + (end-st)/2;
-            if(target == nums[mid]){
-                ans = mid;
-                st = mid + 1;
             }else if(target < nums[mid]){
                 end = mid - 1;
             }else{
@@ -43,11 +27,31 @@ public class FindFirstAndLastPosition{
         return ans;
     }
 
+    public static int findLast(int[] nums, int target){
+        int ans = -1;
+        int st = 0;
+        int end = nums.length-1;
+        while(st <= end){
+            int mid = st + (end - st)/2;
+            if(target == nums[mid]){
+                ans = mid;
+                st = mid + 1;
+            }
+            else if(target < nums[mid]){
+                end = mid - 1;
+            }
+            else{
+                st = mid + 1;
+            }
+        }
+        return ans;
+    }
+
     public static int[] searchRange(int[] nums, int target){
-        int[] result = new int[2];
-        result[0] = findFirst(nums,target);
-        result[1] = findLast(nums,target);
-        return result;
+        int[] res = new int[2];
+        res[0] = findFirst(nums, target);
+        res[1] = findLast(nums, target);
+        return res;
     }
    public static void main(String[] args) {
         int[] nums = {5,7,7,8,8,10};
