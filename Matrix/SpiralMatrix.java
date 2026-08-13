2b1e31646a690eb20bb0764a2f8537bf1ad6e076
Aishwarya <aishpawar696@gmail.com>
2026-08-13 22:20:54 +0530

    Updated the sprial matrix problem from leetcode

--- Matrix/SpiralMatrix.java
+++ Matrix/SpiralMatrix.java
@@ -17,25 +17,30 @@ public class SpiralMatrix {
         int top = 0;
         int bottom = matrix.length - 1;
         while(left <= right && top <= bottom){
+
             for(int i = left ; i <= right ; i++){
                 lst.add(matrix[top][i]);
             }
             top++;
+
             for(int i = top ; i <= bottom ; i++){
                 lst.add(matrix[i][right]);
             }
             right--;
+
             if(top <= bottom){
                 for(int i = right ; i >= left ; i--){
                     lst.add(matrix[bottom][i]);
                 }
             }
+            
             bottom--;
             if(left <= right){
                 for(int i = bottom ; i >= top ; i--){
                     lst.add(matrix[i][left]);
                 }
             }
+
             left++;
         }
         return lst;
