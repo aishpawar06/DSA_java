90b993746a2f83228f55c8b99b78a6a3a63141f8
Aishwarya <aishpawar696@gmail.com>
2026-08-30 18:55:40 +0530

    Revisited valid anagram Problem on leetcode

--- Hashing/DemoValidAnagram.java
+++ Hashing/DemoValidAnagram.java
@@ -1,14 +1,7 @@
-/*
-    Problem: Valid Anagram
-    Platform: LeetCode 242
-    Level: Easy
-    Approach: Frequency Array
-    Time Complexity: O(n)
-    Space Complexity: O(1)
-*/
 package Hashing;
-public class ValidAnagram{
-    public static boolean Valid(String s, String t){
+
+public class DemoValidAnagram {
+    public static boolean validAnagram(String s, String t){
         if(s.length() != t.length()){
             return false;
         }
@@ -17,17 +10,16 @@ public class ValidAnagram{
             arr[s.charAt(i) - 'a']++;
             arr[t.charAt(i) - 'a']--;
         }
-        for(int i = 0 ; i < 26 ; i++){
-            if( arr[i] != 0 ){
+        for(int i = 0 ; i < s.length() ; i++){
+            if(arr[i] != 0){
                 return false;
             }
         }
         return true;
     }
-
     public static void main(String args[]){
         String s = "anagram";
-        String t = "nagaram";
-        System.out.println(Valid(s,t));
+        String t = "naagmraa";
+        System.out.println(validAnagram(s, t));
     }
 }
