afb3606fad580d75b2d0216abdea672dfa1ab9f0
Aishwarya <aishpawar696@gmail.com>
2026-09-01 21:16:44 +0530

    Revisited Binary search on leetcode

--- BinarySearch/BinarySearch.java
+++ BinarySearch/BinarySearch.java
@@ -15,15 +15,9 @@ public class BinarySearch {
         int end = nums.length-1;
         while(st<=end){
             int mid = st + (end - st)/2;
-            if(nums[mid] == target){
-                return mid;
-            }
-            else if(target < nums[mid]){
-                end = mid - 1;
-            }
-            else{
-                st = mid + 1;
-            }
+            if(nums[mid] == target) return mid;
+            else if(target < nums[mid]) end = mid-1;
+            else st=mid+1;
         }
         return -1;
     }
